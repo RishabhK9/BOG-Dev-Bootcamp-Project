ed9643906bdd5acdc2358bee1dacf5b3e3ada813
Rishabh Kanodia <55036111+RishabhK9@users.noreply.github.com>
2023-09-10 10:53:01 -0400

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,12 +1,3 @@
-function toggleBackgroundColor() {
-    var card = document.getElementById('info-card');
-    card.classList.toggle('baby-blue');
-    card.classList.toggle('baby-pink');
-
-    flip-card.classList.toggle('transform');
-
-}
-
 var cards = document.querySelectorAll('.card');
 [...cards].forEach((card)=>{
   card.addEventListener( 'click', function() {

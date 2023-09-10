be5e822a9f49c3a21976aee1085171ad55022859
Rishabh Kanodia <55036111+RishabhK9@users.noreply.github.com>
2023-09-10 11:10:08 -0400

    good

--- functions.js
+++ functions.js
@@ -1,6 +1,16 @@
-var cards = document.querySelectorAll('.card');
-[...cards].forEach((card)=>{
-  card.addEventListener( 'click', function() {
+// var cards = document.querySelectorAll('.card');
+
+const card = document.getElementById('card');
+
+
+function handleFlip(){
     card.classList.toggle('is-flipped');
-  });
-});
+}
+
+card.addEventListener('click', handleFlip);
+
+// [...cards].forEach((card)=>{
+//   card.addEventListener( 'click', function() {
+//     card.classList.toggle('is-flipped');
+//   });
+// });
